bf8c9bc3d2be21ea5a36948feeb49550b7eedcb9
Valdemar Nanon <139665106+valdemarnanon@users.noreply.github.com>
2026-06-22 20:24:02 +0000

    feat: adiciona e exibe Personagem (#samp-001)

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
-
-        .
-        .
-        .
-
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -7,18 +7,19 @@ public class Personagem {
 
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Classe: " + this.classe);
+        System.out.println("Nivel: " + this.nivel);
+        System.out.println("Pontos de vida: " + this.pontosDeVida);
+        System.out.println("Poder base: " + this.poderBase);
     }
 }
 
